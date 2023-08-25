d0993ddbef17c2410c8834b85e8d6fd3f5054f5d
Roushan-NucleusTeq <roushan.kumar@nucleusteq.com>
2023-08-25 12:38:00 +0530

    Custom pop up and integration of API and UI

--- grievanceUI/src/app/component/Login.js
+++ grievanceUI/src/app/component/Login.js
@@ -1,74 +1,94 @@
-import React, { useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify';
+import React, { useEffect, useState } from 'react'
 import api from '../../assets/axios.js';
 import { useNavigate } from 'react-router-dom';
+import MessageSucess from './MessageSucess.js';
 function Login() {
     const initailLogin = {
-        isSubmiting:false,
-        username:"",
-        password:"",
+        isSubmiting: false,
+        username: "",
+        password: "",
     }
 
     const navigate = useNavigate();
 
     const [userLogin, setUserLogin] = useState(initailLogin);
     const [formError, setFormError] = useState(null);
+    const [show, setShow] = useState("");
+    const [errorMessage,setErrorMessage] = useState("");
 
-    const { isSubmiting, username,password } = userLogin;
+    const { isSubmiting, username, password } = userLogin;
 
-    function inputHandler(e){
-        setUserLogin({...userLogin,[e.target.name]:e.target.value})
+    function inputHandler(e) {
+        setUserLogin({ ...userLogin, [e.target.name]: e.target.value })
     }
-
-
-    const checkAuthentication = async ()=>{
-        const res =  await api.post('api/login/authorization',userLogin)
-        console.log(res.data);
-        if(res.data !== "Invalid credantial")
+    
+    const checkAuthentication = async (e) => {
+        e.preventDefault();
+        if(userLogin.username==='')
         {
-            toast.success("success", {
-                position: toast.POSITION.TOP_CENTER
-            });
+           
+            setErrorMessage("username is requied")
+            setShow("show")
+            return 
+        }
+        if(password==='')
+        {
+            setErrorMessage("password is requied")
+            setShow("show")
+            return 
+        }
+        const res = await api.post('api/login/authorization', userLogin)
+        
+        
+
+        if (res.data !== "Invalid credantial") {
+
             navigate("/admin")
 
         }
-        else
-         {
-            toast.error("invalid", {
-                position: toast.POSITION.TOP_CENTER
-            });
-         }
+        else {
+            console.log(res.data);
+            setErrorMessage("Invalid credential")
+            setShow("show")
+        }
+    }
+    const handleClose = ()=>{
+        setShow("");
     }
     return (
         <>
-            <div className='container form-box'>
+
+           
+
+            <div className='container form-box' >
+            <MessageSucess message={errorMessage} show={show} onClick={handleClose} />
                 <div className='' >
-                    <div className='headline'> 
+                    <div className='headline'>
                         <p className="form-group ">Welcome to</p>
                         <p className="form-group ">Grievance/Feedback</p>
                         <p className="form-group ">Management System</p>
-                        
+
                     </div>
-                    <div> 
+                    <div>
                         <label className="form-group formName">Sign in</label>
-                        
+
                     </div>
                     <div className="form-group">
                         <label htmlFor="username" className="labelbox">Username</label>
-                        <input type="text" className="inputBox" id="username" name="username" value={username} disabled={isSubmiting} onChange={(e)=>{inputHandler(e)}}/>
+                        <input type="text" className="inputBox" id="username" name="username" value={username} disabled={isSubmiting} onChange={(e) => { inputHandler(e) }} />
                         <div id="usernameHelp" className=""></div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password" className="labelbox" >Password</label>
-                        <input type="password" className="inputBox" id="password" name="password" value={password} disabled={isSubmiting} onChange={(e)=>{inputHandler(e)}} />
+                        <input type="password" className="inputBox" id="password" name="password" value={password} disabled={isSubmiting} onChange={(e) => { inputHandler(e) }} />
                         <div id="passwordHelp" className=""></div>
                     </div>
                     <div className='form-group'>
-                    <button type="submit" className="myBtn" onClick={checkAuthentication}>Submit</button>
+                        <button type="submit" className="myBtn" onClick={checkAuthentication}>Submit</button>
                     </div>
-                    
-                    
-                    <ToastContainer />
+
+
+
                 </div>
             </div>
         </>
